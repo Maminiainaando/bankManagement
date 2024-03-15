23cf0dd66ddecc0dd275ca1013995f73342fefa7
bonarson <hei.bonarson@gmail.com>
2024-03-15 14:53:59 +0300

    add Account service and Account Repository

---
+++ src/main/java/com/bonarson_dev/bonarson_spring_boot/repository/AccountRepository.java
@@ -0,0 +1,12 @@
+package com.bonarson_dev.bonarson_spring_boot.repository;
+
+import com.bonarson_dev.bonarson_spring_boot.model.Account;
+import com.bonarson_dev.bonarson_spring_boot.service.AccountService;
+
+public class AccountRepository implements AccountService {
+
+    @Override
+    public void addAccount(int id, Account account) {
+        
+    }
+}
---
+++ src/main/java/com/bonarson_dev/bonarson_spring_boot/service/AccountService.java
@@ -0,0 +1,7 @@
+package com.bonarson_dev.bonarson_spring_boot.service;
+
+import com.bonarson_dev.bonarson_spring_boot.model.Account;
+
+public interface AccountService {
+    void addAccount(int id, Account account);
+}
