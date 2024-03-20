b9414390e03f16bdf309812baad696ebd84a989f
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-03-21 01:23:10 +0300

    add navBar interface

---
+++ src/Layouts/NavBar/Nav.js
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+
+export default function ButtonAppBar() {
+  return (
+      <AppBar sx={{position:'fixed',left:'24.2%',top:'0.5vh',width:'65%',backgroundColor:'#f0f0f0'}}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2}}
+          >
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+  );
+}
