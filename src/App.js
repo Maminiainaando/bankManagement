19bd1193b0e176a1d1205df7991f18e44a4c6a9d
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-03-21 22:02:56 +0300

    added more redurection of page

--- src/App.js
+++ src/App.js
@@ -5,7 +5,10 @@ import Table from './Layouts/Layouts';
 import AccountList from './Layouts/Body/TransactionList';
 import TransactionList from './Layouts/Body/TransactionList';
 import AddPersonForm from './Layouts/Body/AddAccount';
+import AddTransactionForm from './Layouts/Body/AddTransaction';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import BalanceViewer from './Components/BalanceNow';
+import BalanceViewerBetweenDate from './testss';
 
 function App() {
   return (
@@ -16,6 +19,9 @@ function App() {
       <Route path="/Account" element={<Table/>}/>
       <Route path="/Transaction" element={<TransactionList/>}/>
       <Route path="/AddPerson" element={<AddPersonForm/>}/>
+      <Route path="/AddTransaction" element={<AddTransactionForm/>}/>
+      <Route path="/bal" element={<BalanceViewer/>}/>
+      <Route path="/balance" element={<BalanceViewerBetweenDate/>}/>
       </Routes>
      </Router>
     </div>
