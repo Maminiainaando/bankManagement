1c95eea8ace09e1b43839d76d960c58a7399800a
hei.marius.3@gmail.com <hei.marius.3@gmail.com>
2024-03-21 12:54:02 +0300

    add Button for the redurection of balance

--- src/Layouts/NavBar/Nav.js
+++ src/Layouts/NavBar/Nav.js
@@ -5,20 +5,25 @@ import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Button from '@mui/material/Button';
 import IconButton from '@mui/material/IconButton';
+import Link from '@mui/material/Link';
 
 export default function ButtonAppBar() {
   return (
-      <AppBar sx={{position:'fixed',left:'24.2%',top:'0.5vh',width:'65%',backgroundColor:'#f0f0f0'}}>
-        <Toolbar>
-          <IconButton
-            size="large"
-            edge="start"
-            color="inherit"
-            aria-label="menu"
-            sx={{ mr: 2}}
-          >
-          </IconButton>
-        </Toolbar>
-      </AppBar>
+    <AppBar sx={{ position: 'fixed', left: '18%', top: '0.5vh', width: '65%', backgroundColor: '#f0f0f0' }}>
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="menu"
+          sx={{ mr: 2 }}
+        />
+        <Button>
+          <Link href="/" underline="none" color={"black"}>
+            {"Balance of Account"}
+          </Link>
+        </Button>
+      </Toolbar>
+    </AppBar>
   );
 }
